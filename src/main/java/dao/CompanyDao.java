b5066448cb3c0f344c750445b09c2b59a24e2e16
yash <yash@yash-PC>
2016-02-03 23:33:58 +0800

    up

--- src/main/java/dao/CompanyDao.java
+++ src/main/java/dao/CompanyDao.java
@@ -8,23 +8,49 @@ import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
 import entity.CompanyEntity;
-import entity.UserEntity;
+
 
 @Repository
 public class CompanyDao {
 	
+
+
+
 	@PersistenceContext
 	private EntityManager em;
+
 	 
 	public void persist(CompanyEntity Company) {
         em.persist(Company);
     }
 	
 
-	public List<CompanyEntity> findAll(){
+	public List<CompanyEntity> compAll(){
 		return (List<CompanyEntity>) em.createQuery("SELECT * FROM company ", CompanyEntity.class).getResultList();
 	}
 	
+	@SuppressWarnings("unchecked")
+	public List<CompanyEntity> compByEin(int Ein) {
+	    return (List<CompanyEntity>) em.createQuery("from company  where COMPANY_EIN = :Ein").getSingleResult();
+		
+	}
 	
+
+	@SuppressWarnings("unchecked")
+	public List<CompanyEntity> compByPhome(int phome) {
+		return (List<CompanyEntity>) em.createQuery("from company  where CONTACT_NUMBER = :phome").getSingleResult();
+		
+	}
+	
+	
+	public int updateUserCoin(Long userId, Long coin) {
+		Query query = getSession().createQuery("update UserEntity set COINS = :coin where ID = :userId");
+		query.setLong("coin",coin);
+        query.setLong("userId",userId);
+        return query.executeUpdate();
+	}
+
+
+
 	
 }
