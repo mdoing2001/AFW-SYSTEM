e8526aa8c9834a9c026956320da76b4ba514b72c
yash <yash@yash-PC>
2016-03-01 23:52:19 +0800

    up

--- src/main/java/tw/com/afw/controller/CompanyController.java
+++ src/main/java/tw/com/afw/controller/CompanyController.java
@@ -1,8 +1,16 @@
 package tw.com.afw.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import tw.com.afw.entity.CompanyEntity;
 import tw.com.afw.service.CompanyService;
 
 
@@ -12,7 +20,23 @@ public class CompanyController {
 	
 	
 	 @Autowired
-	 private CompanyService service;
+	 private CompanyService CompanyService;
+	 
+	 
+	 
+	    //-------------------Create a User--------------------------------------------------------
+     
+	    @RequestMapping(value = "/XXX", method = RequestMethod.POST)
+	    public ResponseEntity<Void> createUser(@RequestBody CompanyEntity company,    UriComponentsBuilder ucBuilder) {
+	        //System.out.println("Creating Company " + company.getCompany_Name());
+
+	        CompanyService.ins(company);
+	     
+	        HttpHeaders headers = new HttpHeaders();
+	        headers.setLocation(ucBuilder.path("/xxx").buildAndExpand(company.getCompany_Id()).toUri());
+	        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+	    }
+	 
 	 
 	 
 	 
