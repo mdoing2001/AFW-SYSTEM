49dde6082dbcd411b38e5f6d9615558076960167
yash <yash@yash-PC>
2016-03-03 23:50:58 +0800

    up

--- src/main/java/tw/com/afw/controller/CompanyController.java
+++ src/main/java/tw/com/afw/controller/CompanyController.java
@@ -1,5 +1,8 @@
 package tw.com.afw.controller;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -10,8 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import tw.com.afw.entity.AccountancyEntity;
 import tw.com.afw.entity.CompanyEntity;
+import tw.com.afw.entity.ContractEntity;
 import tw.com.afw.service.CompanyService;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 
 @Controller
@@ -26,15 +34,42 @@ public class CompanyController {
 	 
 	    //-------------------Create a User--------------------------------------------------------
      
-	    @RequestMapping(value = "/XXX", method = RequestMethod.POST)
-	    public ResponseEntity<Void> createUser(@RequestBody CompanyEntity company,    UriComponentsBuilder ucBuilder) {
+	    @RequestMapping(value = "/new/company/add", method = RequestMethod.POST)
+	    public String createUser(@RequestBody String newCompanyJson) {
 	        //System.out.println("Creating Company " + company.getCompany_Name());
 
-	        CompanyService.ins(company);
-	     
-	        HttpHeaders headers = new HttpHeaders();
-	        headers.setLocation(ucBuilder.path("/xxx").buildAndExpand(company.getCompany_Id()).toUri());
-	        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+	        //CompanyService.ins(companyj);
+	    	try {
+				JSONObject obj = (JSONObject) new JSONParser().parse(newCompanyJson);
+				
+				String companyStr = obj.get("company").toString();
+				String contractStr = obj.get("contract").toString();
+				String accStr = obj.get("acc").toString();
+				
+				CompanyEntity companyEntity = new Gson().fromJson(companyStr, CompanyEntity.class);
+				ContractEntity contractEntity = new Gson().fromJson(contractStr, ContractEntity.class);
+				AccountancyEntity accountancyEntity = new Gson().fromJson(accStr, AccountancyEntity.class);
+				
+				companyEntity.setAcc_Id(accountancyEntity);
+				contractEntity.setCompany_id(companyEntity);
+				
+				//companyEntity.getAcc_Id().getAcc_name();
+				
+				
+			} catch (JsonSyntaxException e) {
+				e.printStackTrace();
+				//{"status":"fail","message":e.printStackTrace();}
+				return null;
+			} catch (ParseException e) {
+				e.printStackTrace();
+				//{"status":"fail","message":e.printStackTrace();}
+				return null;
+			}
+
+	    	
+	       // HttpHeaders headers = new HttpHeaders();
+	        //headers.setLocation(ucBuilder.path("/xxx").buildAndExpand(company.getCompany_Id()).toUri());
+	        return "/xxxx";
 	    }
 	 
 	 
